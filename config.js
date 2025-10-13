a209c9bfc4c95e0975384f13dfe6b5dd1268184b
thiaguinhosolucoesia-tech <thiaguinhosolucoesia@gmail.com>
2025-10-13 19:15:58 -0300

    Update and rename admin_test.html to config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+// Este arquivo armazena "segredos" da aplicação de forma ofuscada.
+// Não é segurança criptográfica, mas impede a visualização casual.
+
+const AppConfig = {
+    // A string '*177' codificada em Base64
+    ADMIN_TRIGGER_KEY: 'KjE3Nw==' 
+};
