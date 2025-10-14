d4cf88c3fd9b0b8b2f78f324ab38f4b7de64baa3
thiaguinhosolucoesia-tech <thiaguinhosolucoesia@gmail.com>
2025-10-13 22:05:25 -0300

    Atualizar o config.js

--- config.js
+++ config.js
@@ -3,5 +3,14 @@
 
 const AppConfig = {
     // A string '*177' codificada em Base64
-    ADMIN_TRIGGER_KEY: 'KjE3Nw==' 
+    ADMIN_TRIGGER_KEY: 'KjE3Nw==',
+    firebaseConfig: {
+        apiKey: "AIzaSyA9WEvRwaDuEDFlTlIwPxKvNr6hcNlYFbI",
+        authDomain: "thiaguinho-solucoes.firebaseapp.com",
+        databaseURL: "https://thiaguinho-solucoes-default-rtdb.firebaseio.com",
+        projectId: "thiaguinho-solucoes",
+        storageBucket: "thiaguinho-solucoes.firebasestorage.app",
+        messagingSenderId: "879610267391",
+        appId: "1:879610267391:web:bc48a54e0ce8e541feeeca"
+    }
 };
