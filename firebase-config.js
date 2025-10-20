2f0677c21ee628a235e91b8f60486b9b91b0ebab
thiaguinhosolucoesia-tech <thiaguinhosolucoesia@gmail.com>
2025-10-20 20:00:01 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,14 +4,14 @@
 
 // ATENÇÃO: ESTE É O SEU ARQUIVO DE CONFIGURAÇÃO REAL E FUNCIONAL.
 const firebaseConfig = {
-    apiKey: "AIzaSyA9WEvRwaDuEDFlTlIwPxKvNr6hcNlYFbI",
-    authDomain: "thiaguinho-solucoes.firebaseapp.com",
-    databaseURL: "https://thiaguinho-solucoes-default-rtdb.firebaseio.com",
-    projectId: "thiaguinho-solucoes",
-    storageBucket: "thiaguinho-solucoes.firebasestorage.app",
-    messagingSenderId: "879610267391",
-    appId: "1:879610267391:web:bc48a54e0ce8e541feeeca"
-};
+    apiKey: "AIzaSyCHh2XM92NE99Hgj0caW_7YQZCsQnjNU8U",
+    authDomain: "thiaguinhoplataforma.firebaseapp.com",
+    databaseURL: "https://thiaguinhoplataforma-default-rtdb.firebaseio.com",
+    projectId: "thiaguinhoplataforma",
+    storageBucket: "thiaguinhoplataforma.firebasestorage.app",
+    messagingSenderId: "724302646935",
+    appId: "1:724302646935:web:0debbe25fa0aad055323d2"
+  };
 
 // O CÓDIGO DE ACESSO DO ADMIN (codificado em Base64 para '*177')
 const ADMIN_TRIGGER_KEY = 'KjE3Nw==';
